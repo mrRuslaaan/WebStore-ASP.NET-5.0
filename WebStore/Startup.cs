ddc65e70e769598b597be67e1e9c2f6368b38fa4
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-12-01 13:35:14 +0300

    configurated Greeting

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -3,11 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebStore
 {
     public class Startup
     {
+        private readonly IConfiguration _Configuration;
+
+        public Startup(IConfiguration Configuration) => _Configuration = Configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -21,11 +26,13 @@ namespace WebStore
 
             app.UseRouting();
 
+            var greeting = _Configuration["Greetings"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(greeting);
                 });
             });
         }
