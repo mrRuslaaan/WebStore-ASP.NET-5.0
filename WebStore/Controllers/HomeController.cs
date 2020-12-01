ebd1d1f26791fb478cf2af8cb22cdd694ac9d845
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-12-01 15:25:36 +0300

    transfer data from model to view through controller

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 
@@ -7,6 +9,14 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
+
+        private static readonly List<Worker> __Workers = new()
+        {
+            new Worker { Id = 1, LastName = "Пушкин", FirstName = "Александр", Patronymic = "Сергеевич", Age = 22 },
+            new Worker { Id = 2, LastName = "Достоевский", FirstName = "Фёдор", Patronymic = "Михайлович", Age = 39 },
+            new Worker { Id = 3, LastName = "Толстой", FirstName = "Лев", Patronymic = "Николаевич", Age = 45 },
+        };
+
         public HomeController(IConfiguration Configiration) => _Configuration = Configiration; 
         
         public IActionResult Index()
@@ -17,6 +27,11 @@ namespace WebStore.Controllers
         public IActionResult Index_2()
         {
             return Content(_Configuration["TextForFirstControllerSecondAction"]);
+        }
+
+        public IActionResult Workers() 
+        {
+            return View(__Workers);
         }
     }
 }
