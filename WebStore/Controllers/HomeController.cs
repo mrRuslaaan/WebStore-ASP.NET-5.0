f6e90109783bad749a3463cf7c869a72b408edfe
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-12-02 15:49:58 +0300

    made func AdditionalInformaton

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace WebStore.Controllers
 
         private static readonly List<Worker> __Workers = new()
         {
-            new Worker { Id = 1, LastName = "Пушкин", FirstName = "Александр", Patronymic = "Сергеевич", Age = 22 },
-            new Worker { Id = 2, LastName = "Достоевский", FirstName = "Фёдор", Patronymic = "Михайлович", Age = 39 },
-            new Worker { Id = 3, LastName = "Толстой", FirstName = "Лев", Patronymic = "Николаевич", Age = 45 },
+            new Worker { Id = 1, LastName = "Пушкин", FirstName = "Александр", Patronymic = "Сергеевич", Age = 22, Position = "junior developer" },
+            new Worker { Id = 2, LastName = "Достоевский", FirstName = "Фёдор", Patronymic = "Михайлович", Age = 39, Position = "designer" },
+            new Worker { Id = 3, LastName = "Толстой", FirstName = "Лев", Patronymic = "Николаевич", Age = 45, Position = "senior developer"},
         };
 
         public HomeController(IConfiguration Configiration) => _Configuration = Configiration; 
@@ -32,6 +32,13 @@ namespace WebStore.Controllers
         public IActionResult Workers() 
         {
             return View(__Workers);
+        }
+
+        public IActionResult AdditionalInformation(int id)
+        {
+            var worker = __Workers[id - 1];
+
+            return View();
         }
     }
 }
