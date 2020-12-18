2e5b54ab03118d5cbc21166c3f238360e73c9d13
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-12-18 16:46:03 +0300

    Role and User

---
+++ WebStore.Domain/Entityes/Identity/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entityes.Identity
+{
+    class Role : IdentityRole
+    {
+        public const string Administrator = "Administrators";
+
+        public const string User = "Users";
+    }
+}
