7fd98ad4e122e7459a501d9a708e4ac047112189
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-12-22 14:48:11 +0300

    correct mistakes

--- WebStore/wwwroot/js/site.js
+++ WebStore/wwwroot/js/site.js
@@ -1,7 +1,7 @@
-jQuery(function($) {
+jQuery(function($) {'use strict',
 
 	var form = $('.contact-form');
-	form.submit(function () {
+	form.submit(function () {'use strict',
 		$this = $(this);
 		$.post("sendemail.php", $(".contact-form").serialize(),function(result){
 			if(result.type == 'success'){
